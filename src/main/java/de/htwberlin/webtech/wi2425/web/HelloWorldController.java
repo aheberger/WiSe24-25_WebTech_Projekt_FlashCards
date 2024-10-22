eddd204242b3686f8e62335a9cadb0caa629ac29
MyLinhChu <linhchu97@gmail.com>
2024-10-22 13:51:22 +0200

    1. commit

---
+++ src/main/java/de/htwberlin/webtech/wi2425/web/HelloWorldController.java
@@ -0,0 +1,12 @@
+package de.htwberlin.webtech.wi2425.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+
+//    @GetMapping
+
+
+}
